ed505677618db32f1cbfbb5d39f3eac3557d0fc7
Gabriel <gabrielzion2008@gmail.com>
2025-02-16 01:28:19 -0600

    changed ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyElevate",
-    version="0.1.0",
+    version="0.1.1",
     author="Gabrielzv1233",
     author_email="gabrielzv1233@gmail.com",
     url="https://github.com/gabrielzv1233/PyElevate",
